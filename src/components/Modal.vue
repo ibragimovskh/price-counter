afdcc21dd5f3f133b6477728e44a0a894bc2cadd
Shohrux <dotanoobbich@mail.ru>
2019-07-16 22:18:10 +0500

    Checbox list

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,8 +1,35 @@
 <template>
   <div>
     <!-- eslint-disable -->
-    <b-modal v-for="modal in modals" :id="modal.id" :title="modal.title" :key="modal.id" size="xl">
-      <span>Данный расчет не даёт точной оценки, но позволяет определить общий бюджет и нижнюю границу стоимости разработки сайта.</span>
+    <b-modal id="landing" title="Landing Page" size="xl">
+      <b-container>
+        <b-row>
+          <b-col>
+            <h3 class="modal__title">Модули:</h3>
+            <b-form-group>
+              <b-form-checkbox-group v-model="landing.price">
+                <b-form-checkbox
+                  v-for="module in landing.modules"
+                  :value="module.price"
+                  :key="module.name"
+                  v-model="landing.selected"
+                >{{landing.selected}}</b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group>
+            {{countPrice}}
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-modal>
+    <b-modal id="business" title="Бизнес-сайт" size="xl">
+      <b-container>
+        <b-row></b-row>
+      </b-container>
+    </b-modal>
+    <b-modal id="store" title="Интернет Магазин" size="xl">
+      <b-container>
+        <b-row></b-row>
+      </b-container>
     </b-modal>
   </div>
 </template>
@@ -11,15 +38,41 @@
 export default {
   data() {
     return {
-      modals: [
-        {
-          id: "landing",
-          title: "Landing Page"
-        },
-        { id: "business", title: "Бизнес-сайт" },
-        { id: "store", title: "Интернет Магазин" }
-      ]
+      landing: {
+        modules: [
+          {
+            name: "Галерея",
+            price: 1500
+          },
+          {
+            name: "Слайдер",
+            price: 2000
+          },
+          {
+            name: "Поиск",
+            price: 3000
+          },
+          {
+            name: "Обратный звонок",
+            price: 4000
+          },
+          {
+            name: "Хостинг",
+            price: 4200
+          },
+          {
+            name: "Форма обратной связи",
+            price: 2100
+          }
+        ],
+        selected: [false, false, false, false, false, false]
+      }
     };
+  },
+  computed: {
+    countPrice() {
+      return this.landing.selected.reduce((a, b) => a + b);
+    }
   }
 };
 </script>
